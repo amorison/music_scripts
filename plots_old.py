aa1eb8ba0d6e562097debd641bd5ead370ec8a4b
Adrien Morison <adrien.morison@gmail.com>
2022-03-25 19:08:47 +0000

    Use ArrayOnGrid in plots_old

--- plots_old.py
+++ plots_old.py
@@ -10,67 +10,61 @@ from pymusic.io.music_new_format import MusicDumpInfo
 from pymusic.big_array.dtyped_func import FixedDtypedFunc
 from pymusic.plotting import SinglePlotFigure
 
-from array_on_grid import DumpArrayOnGrid
+from array_on_grid import DumpArrayOnGrid, SimArrayOnGrid
 from derived_fields import FieldGetter, ProfGetter, TimeAveragedProfGetter
 from plots import SphericalPlot
 from prof1d import Prof1d
 
 
-def _music_sim(folder) -> MusicSim:
-    return MusicSim.from_dump_file_names(
+def _music_sim(folder) -> SimArrayOnGrid:
+    sim = MusicSim.from_dump_file_names(
         sorted(folder.glob('*.music')),
         MusicDumpInfo(num_space_dims=2, num_velocities=2, num_scalars=1),
         [PeriodicArrayBC(), PeriodicArrayBC()])
+    return SimArrayOnGrid(sim)
 
 
 # vrms(r, time) = sqrt(mean_theta(v2))
 # also plot radial profile of time average of vrms(r, time)
-def tau_conv(folder):
+def tau_conv(simog, rcore: float):
     """Convective time scale."""
-    sim = _music_sim(folder)
-    sim_data = sim.big_array()
-    grid = sim.grid
+    grid = simog.grid
     d_rad = grid.r_grid.cell_widths()
-    params = Prof1d(folder / "..").params
-    core_mask = grid.r_grid.cell_centers() < params['rcore']
+    core_mask = grid.r_grid.cell_centers() < rcore
     return (
-        ProfGetter('vel_square')(sim_data)
+        ProfGetter('vel_square')(simog)
         .collapse(FixedDtypedFunc(lambda vrms2: np.sum(d_rad[core_mask] / np.sqrt(vrms2[core_mask])),
                                   np.float64), axis="x1")
         ).array().mean()
 
 
-def tseries(folder, var):
+def tseries(simog, var):
     """Time series of a var from a given folder."""
-    sim = _music_sim(folder)
-
-    grid = sim.grid
+    grid = simog.sim.grid
     d_rad = grid.r_grid.cell_widths()
-    sim_data = sim.big_array()
+    sim_data = simog.data
     rad = grid.r_grid.cell_centers()
     time = np.array(sim_data.labels_along_axis("time"))
     var_series = (
-        ProfGetter(var)(sim_data)
+        ProfGetter(var)(simog)
         .collapse(FixedDtypedFunc(lambda w: np.average(w, weights=d_rad * rad**2), np.float64), axis="x1")
     ).array()
     return time, var_series
 
 
-def prof(var, folder):
+def prof(simog, var):
     """Time averaged profile."""
-    sim = _music_sim(folder)
-    sim_data = sim.big_array()
-    var_prof = TimeAveragedProfGetter(var)(sim_data).array()
-    return sim.grid.r_grid.cell_centers(), var_prof
+    var_prof = TimeAveragedProfGetter(var)(simog).array()
+    return simog.sim.grid.r_grid.cell_centers(), var_prof
 
 
-def plot_prof(var, *folders):
+def plot_prof(simog, var):
     """Plot radial profile of density."""
     figdir = Path('figures')
     figdir.mkdir(parents=True, exist_ok=True)
-    for folder in folders:
-        rad, var_prof = prof(var, folder)
-        plt.semilogy(rad, var_prof, label=folder.name)
+
+    rad, var_prof = prof(simog, var)
+    plt.semilogy(rad, var_prof)
     plt.xlabel('radius')
     plt.ylabel(var)
     plt.legend()
@@ -78,40 +72,40 @@ def plot_prof(var, *folders):
     plt.close()
 
 
-def plot_dprof(var, *folders):
+def plot_dprof(simog, var):
     """Plot radial gradient profile of var."""
     figdir = Path("figures")
     figdir.mkdir(parents=True, exist_ok=True)
-    for folder in folders:
-        rad, var_prof = prof(var, folder)
-        grad = (var_prof[1:] - var_prof[:-1]) / (rad[1:] - rad[:-1])
-        rad_grad = (rad[1:] + rad[:-1]) / 2
-        plt.plot(rad_grad, grad, label=folder.name)
-        plt.legend()
+
+    rad, var_prof = prof(simog, var)
+    grad = (var_prof[1:] - var_prof[:-1]) / (rad[1:] - rad[:-1])
+    rad_grad = (rad[1:] + rad[:-1]) / 2
+    plt.plot(rad_grad, grad)
+    plt.legend()
+
     plt.xlabel('radius')
     plt.ylabel(var)
     plt.savefig(figdir / f'{var}_grad_prof.pdf', bbox_inches='tight')
     plt.close()
 
 
-def plot_tseries(folder, var):
+def plot_tseries(simog, var):
     """Plot time series."""
     figdir = Path('figures')
     figdir.mkdir(parents=True, exist_ok=True)
-    time, data = tseries(folder, var)
-    plt.plot(time, data, label=folder.name)
+    time, data = tseries(simog, var)
+    plt.plot(time, data)
     plt.legend()
     plt.savefig(figdir / f"tseries_{var}.pdf", bbox_inches='tight')
     plt.close()
 
 
-def plot_var(folder, var, vel_arrows=False):
+def plot_var(simog: SimArrayOnGrid, var, vel_arrows=False):
     """Field plots in a given folder."""
     figdir = Path("figures")
     figdir.mkdir(parents=True, exist_ok=True)
-    sim = _music_sim(folder)
 
-    for i, dump in enumerate(sim.dumps):
+    for i, dump in enumerate(simog.sim.dumps):
         fig = SinglePlotFigure(
             plot=SphericalPlot(
                 dump=DumpArrayOnGrid(dump),
@@ -124,23 +118,20 @@ def plot_var(folder, var, vel_arrows=False):
 
 if __name__ == "__main__":
     simfold = Path("transient")
-    plot_var(simfold, 'e_int', vel_arrows=True)
-    #plot_var(Path(), 'rho', vel_arrows=False)
-    plot_tseries(simfold, "v2")
-    plot_tseries(simfold, "vr2")
-    plot_tseries(simfold, "vt2")
-    plot_tseries(simfold, "vel_2")
-    plot_var(simfold, "vel_2")
-    plot_prof("vel_2", simfold)
-    #plot_prof('rho', *sim_folders)
-    #plot_prof('scalar_1', *sim_folders)
-    #plot_prof('ekin', *sim_folders)
-    #plot_prof('v2', *sim_folders)
-    #plot_prof('vr2', Path('transient'))
-    #plot_var(Path('no_es'), 'vr2')
-    #plot_var(Path('no_es'), 'v2')
-    #plot_var(Path('transient'), 'vr2_ov2')
-    #plot_var(Path('no_es'), 'vt2_ov2')
-    #print(*(f'Conv time {fold.name}: {tau_conv(fold):.2e}'
-    #        for fold in sim_folders),
-    #      sep='\n')
+    simog = _music_sim(simfold)
+    compute_tconv = False
+
+    plot_var(simog, 'e_int', vel_arrows=True)
+    plot_var(simog, "vel_2")
+
+    plot_prof(simog, "vel_2")
+
+    plot_tseries(simog, "v2")
+    plot_tseries(simog, "vr2")
+    plot_tseries(simog, "vt2")
+    plot_tseries(simog, "vel_2")
+
+    if compute_tconv:
+        profs1d = Prof1d(simfold / "..")
+        tconv = tau_conv(simog, profs1d.params["rcore"])
+        print(f'Conv time {simfold.name}: {tconv:.2e}')
