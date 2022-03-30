9a7fa1727251cf34904195c3cb7d303c092b3bff
Adrien Morison <adrien.morison@gmail.com>
2022-03-30 15:33:42 +0100

    Add r_schwarz series plot

--- music_scripts/plot_pendepth.py
+++ music_scripts/plot_pendepth.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import typing
 from dataclasses import dataclass
+from pathlib import Path
 
 import h5py
 import numpy as np
@@ -47,6 +48,22 @@ class ContourPlot(Plot):
                 label=self.contour.name)
 
 
+@dataclass(frozen=True)
+class SchwarzSeries:
+    values: np.ndarray
+    time: np.ndarray
+
+
+@dataclass(frozen=True)
+class SchwarzSeriesPlot(Plot):
+    series: SchwarzSeries
+
+    def draw_on(self, ax) -> None:
+        ax.plot(self.series.time, self.series.values)
+        ax.set_xlabel("time")
+        ax.set_ylabel("Schwarzschild radius")
+
+
 def get_contour(h5file: str, idump: int, name: str) -> Contour:
     with h5py.File(h5file) as h5f:
         chkp = h5f["checkpoints"][f"{idump:05d}"]
@@ -58,9 +75,21 @@ def get_contour(h5file: str, idump: int, name: str) -> Contour:
     return data
 
 
+def schwarz_series_in_file(h5file: Path) -> SchwarzSeries:
+    with h5py.File(h5file) as h5f:
+        checks = h5f["checkpoints"]
+        time = np.zeros(len(checks))
+        values = np.zeroes(len(checks))
+        for i, check in enumerate(checks.values()):
+            time[i] = check["parameters"]["time"][()].item()
+            values[i] = check["pp_parameters"]["ave_r_schwarz_max"][()].item()
+    return SchwarzSeries(values, time)
+
+
 def cmd(conf: ConfigurationManager) -> None:
-    idump = 7874
-    h5file = "post_es.h5"
+    idump = 7800
+    h5file = Path("post_es.h5")
+
     fig = SinglePlotFigure(
         plot=SameAxesPlot(
             plots=(ContourPlot(get_contour(h5file, idump, var))
@@ -69,3 +98,8 @@ def cmd(conf: ConfigurationManager) -> None:
         ),
     )
     fig.save_to("pendepth.pdf")
+
+    fig = SinglePlotFigure(
+        plot=SchwarzSeriesPlot(schwarz_series_in_file(h5file)),
+    )
+    fig.save_to("series_r_schwarz.pdf")
