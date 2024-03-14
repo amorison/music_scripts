6d773dedb0a84d9a7bc04fe620e3925420617304
Adrien Morison <adrien.morison@gmail.com>
2024-03-14 17:09:57 +0000

    Fix type annotation for older Python versions

--- music_scripts/config.py
+++ music_scripts/config.py
@@ -87,7 +87,7 @@ class River(Section):
 @dataclass
 class Vtk(Section):
     vtk_dir: Path = path_entry(path="vtk", cli_short="V", doc="output directory")
-    extra_vars: tuple[str, ...] = TupleEntry(str).entry(
+    extra_vars: Tuple[str, ...] = TupleEntry(str).entry(
         doc="additional variables in VTK file", cli_short="E"
     )
 
